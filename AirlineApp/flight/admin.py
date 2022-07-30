eac39829ea9ac72e41790b1d902351eea00f11d5
mdparvex <mdparvex443@gmail.com>
2022-07-30 14:24:15 +0600

    modified code in user app

--- AirlineApp/flight/admin.py
+++ AirlineApp/flight/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from flight.models import Airport, Flight
+from flight.models import Airport, Flight, Passenger
 
 # Register your models here.
+class FlightAdmin(admin.ModelAdmin):
+	list_display = ("id", "origin", "destination", "duration")
+
+class PassengerAdmin(admin.ModelAdmin):
+	filter_horizontal = ("flights",)
+
 admin.site.register(Airport)
-admin.site.register(Flight)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
--- AirlineApp/flight/views.py
+++ AirlineApp/flight/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Flight, Airport
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Flight, Airport, Passenger
 
 # Create your views here.
 
@@ -8,3 +10,19 @@ def index(request):
 		"flights": Flight.objects.all()
 	}
 	return render(request, "flight/index.html", context)
+
+def flight(request, flight_id):
+	flight = Flight.objects.get(pk=flight_id)
+	context = {
+		"flight": flight,
+		"passengers": flight.passengers.all(),
+		"non_passengers": Passenger.objects.exclude(flights=flight).all()
+	}
+	return render(request, "flight/flight.html", context)
+
+def book(request, flight_id):
+	if request.method =="POST":
+		flight = Flight.objects.get(pk=flight_id)
+		passenger = Passenger.objects.get(pk = int(request.POST["passenger"]))
+		passenger.flights.add(flight)
+		return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
