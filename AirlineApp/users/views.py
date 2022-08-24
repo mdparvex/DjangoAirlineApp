c60e91c24a90df90b64569614f7e0fedcec95205
mdparvex <mdparvex443@gmail.com>
2022-08-24 22:32:50 +0600

    little changed

--- AirlineApp/users/views.py
+++ AirlineApp/users/views.py
@@ -29,7 +29,7 @@ def login_view(request):
 		user = authenticate(request, username = username, password=password)
 		if user is not None:
 			login(request, user)
-			return HttpResponseRedirect(reverse("user_index"))
+			return HttpResponseRedirect(reverse("index"))
 		else:
 			return render(request, "users/login.html", {"message": 'invalid credintials'})
 	return render(request, "users/login.html")
